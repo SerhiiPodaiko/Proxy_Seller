c0c87661d7839f10bde80bec3c974e5260101000
SerhiiPodaiko <podajko1997@gmail.com>
2023-10-11 19:33:40 +0300

    Added bootstrap, axios, sass, svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const process = require('process')
+const path = require('path')
+
+const {
+    NEXT_PUBLIC_API_URL,
+    NEXT_DEV_API_URL,
+} = process.env
+
+const nextConfig = {
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        })
+
+        return config
+    },
+    env: {
+        NEXT_PUBLIC_API_URL,
+        NEXT_DEV_API_URL,
+    },
+    async headers() {
+        return [
+            {
+                source: '/favicon.ico',
+                headers: [
+                    {
+                        key: 'Link',
+                        value: '/favicon.jpeg',
+                    },
+                ],
+            },
+        ]
+    }
+}
 
 module.exports = nextConfig
